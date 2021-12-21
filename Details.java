787e01856fbbd255c55b943b1a8c8b103c43cd46
Anshul Malviya <88474736+anshulmalviya1@users.noreply.github.com>
2021-12-21 16:51:25 +0530

    Add files via upload

---
+++ Details.java
@@ -0,0 +1,91 @@
+/*
+SYSTEM DESCRIPTION:
+The proposed system is for making it easier to manage policyholder details, agent details, policy details, claimant details and payment details. So this will be developed for managing the insurance management system. The overall system is controlled through the main menu.
+
+The main menu contains 6 parts.
+
+1. Policy Schemes.
+
+2. Agent Login.
+
+3. Customer Login.
+
+4. Administrator Login.
+
+5. About us.
+
+6. Contact us.
+
+POLICY SCHEMES:-
+Various policy schemes are:-
+
+1. LIFE INSURANCE:-
+a) Whole life policy
+
+b) Term life policy
+
+c) Endownment policy
+
+d) Pension plans
+
+2. GENERAL INSURANCE:-
+a) Home insurance
+
+b) Auto insurance
+
+c) Fire insurance
+
+AGENT LOGIN
+The agent login form links to-
+
+1. Basic agent information like contact details and address will be shown in the customer insurance information window.
+
+2. All the information related to insurances which he has made to his clients.
+
+3. Commission received by him for each insurance made by him respectively.
+
+4. Option to create a new policy for any existing/new client.
+
+5. Option to edit the contact information of its client.
+
+6. Option to delete a policy of any client in case of policy lapse.
+
+CUSTOMER LOGIN:-
+The form contains the agent information like-
+
+1. Personal information required by the insurance agents.
+
+2. Next premium due of respective insurances by the client along with maturity date, agent info etc.
+
+ADMINISTRATOR LOGIN:-
+The administrator has rights to-
+
+1. Create a new agent
+
+2. Edit agent’s information and its commission percentage.
+
+3. Delete an agent’s database and all its policies respectively.
+
+ABOUT US:-
+It contains information about the organization’s history and its achievements.
+
+CONTACT US:-
+It contains the contact details of the organization’s various branches located in different parts of a country.
+
+REPORTS:
+• Sales report
+
+• Claimant information report
+
+• Agent information report
+
+• Client information report
+ */
+
+/**
+ *
+ * @author ACER
+ */
+public class Details {
+    
+}
